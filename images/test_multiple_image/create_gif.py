3349c7c8e7eb8e04b83e3706e735e5bbeecf4282
Diane Béranger <61918652+Git-Diane@users.noreply.github.com>
2021-03-15 20:44:18 +0100

    Update create_gif.py

--- images/test_multiple_image/create_gif.py
+++ images/test_multiple_image/create_gif.py
@@ -6,6 +6,7 @@ np.random.seed(0)
 height = 100
 width = 100
 num = 50
+nb_images=np.arange(1,num+1,5)
 
 #if (len(sys.argv) < 3):
 #    print >> sys.stderr, "ERROR. Usage : XXX.sh height_min step"
@@ -18,10 +19,11 @@ color_1 = (255, 255, 255)
 rand = np.random.randint(256, size=(num, width, height, 3))
 images = []
 
-for l in range(num):
-    im = Image.new('RGB', (width, height), color_1)
-    for i in range(width):
-        for j in range(height):
-            im.putpixel((i,j), (rand[l][i][j][0], rand[l][i][j][1], rand[l][i][j][2]))
-    images.append(im)
-images[0].save('./{}x{}x{}.gif'.format(num, width, height), save_all=True, append_images=images[1:], optimize=False, duration=100, loop=0)
+for k in range(len(nb_images)):
+    for l in range(nb_images[k]):
+        im = Image.new('RGB', (width, height), color_1)
+        for i in range(width):
+            for j in range(height):
+                im.putpixel((i,j), (rand[l][i][j][0], rand[l][i][j][1], rand[l][i][j][2]))
+        images.append(im)
+        images[0].save('./{}x{}x{}.gif'.format(nb_images[k], width, height), save_all=True, append_images=images[1:], optimize=False, duration=100, loop=0)
