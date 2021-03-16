66c77a60a8e15f6cd0231391b8a4f09aa5f3e2c2
pinnertw <peng-wei.chen@polytechnique.edu>
2021-03-16 13:49:33 +0100

    Change demons

--- demonstration.sh
+++ demonstration.sh
@@ -13,45 +13,35 @@ echo "Campusplan : 1x10000x7000"
 echo "Mandelbrot : 20x1200x1200"
 
 echo "1 machine 1 process, Sequentiel"
-time {
     for i in $INPUT_DIR/*gif ; do
         DEST=$OUTPUT_DIR/`basename $i .gif`.gif
         echo -n "$i "
         ./sobelf $i $DEST 0 2> /dev/null
     done
-}
-echo "3 machine 18 process, MPI"
-time {
+echo "3 machine 6 process, MPI"
     for i in $INPUT_DIR/*gif ; do
         DEST=$OUTPUT_DIR/`basename $i .gif`.gif
         echo -n "$i "
         salloc -N 3 -n 18 mpirun ./sobelf $i $DEST 3 2> /dev/null
     done
-}
 
-echo "3 machine 18 process, Merge"
-time {
+echo "3 machine 6 process, Merge"
     for i in $INPUT_DIR/*gif ; do
         DEST=$OUTPUT_DIR/`basename $i .gif`.gif
         echo -n "$i "
         salloc -N 3 -n 18 mpirun ./sobelf $i $DEST 6 2> /dev/null
     done
-}
 
 echo "1 machine 6 threads, OpenMP"
-time {
     for i in $INPUT_DIR/*gif ; do
         DEST=$OUTPUT_DIR/`basename $i .gif`.gif
         echo -n "$i "
         ./sobelf $i $DEST 1 2> /dev/null
     done
-}
 
 echo "1 machine 1 process 6 threads , Merge"
-time {
     for i in $INPUT_DIR/*gif ; do
         DEST=$OUTPUT_DIR/`basename $i .gif`.gif
         echo -n "$i "
         salloc -N 1 -n 1 mpirun ./sobelf $i $DEST 6 2> /dev/null
     done
-}
